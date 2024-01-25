674457b20458ae0375a4b4242e72f5662b079f0b
jchaverot <jeremy.chaverot@epfl.ch>
2024-01-25 23:11:16 +0100

    some updates

--- roadmap/post_semester/rename.py
+++ roadmap/post_semester/rename.py
@@ -2,11 +2,11 @@ import os
 
 def rename_and_delete_images(folder_path):
     # List all files in the folder
-    files = sorted([f for f in os.listdir(folder_path) if f.endswith('.npy') and not f.startswith('._')])
+    files = sorted([f for f in os.listdir(folder_path) if f.endswith('.jpg') and not f.startswith('._')])
 
     # Pad the index with zeros up to 4 digits, rename files, and then delete the old ones
     for index, file in enumerate(files):
-        new_filename = f"pose{str(index)}.npy"
+        new_filename = f"{int(index):06}.jpg"
         old_file_path = os.path.join(folder_path, file)
         new_file_path = os.path.join(folder_path, new_filename)
 
@@ -16,6 +16,6 @@ def rename_and_delete_images(folder_path):
 
 
 # Update this path to the folder containing your .jpg images
-folder_path = r"F:\bachelor-thesis\Gen6D\data\SpaceCraft\jwst-ref\pose"
+folder_path = r"/Volumes/Crucial X6/bachelor-thesis/Gen6D/data/SpaceCraft/swisscube-test/images"
 rename_and_delete_images(folder_path)
 
